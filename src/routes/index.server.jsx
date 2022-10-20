3e8513e47508f336786ed27e2903f30530320eb7
Amanda Hultén <amanda@avitusit.se>
2022-10-20 11:30:21 +0200

    Trying to get products on startpage

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,4 +1,5 @@
 import { Suspense } from 'react';
+import { useRouteParams, useShopQuery, gql, Link } from '@shopify/hydrogen';
 
 // Components
 import { Layout } from '../components/Layout.server';
@@ -6,13 +7,46 @@ import Hero from '../components/global/Hero.server';
 import FeaturedCollections from '../components/global/FeaturedCollections.server';
 
 export default function Home() {
+  const { handle } = useRouteParams();
+
+  const {
+    data: { products },
+  } = useShopQuery({
+    query: AllProducts
+  });
+
   return (
     <Layout>
       <Hero />
       <Suspense fallback={`Loading content...`}>
-
         <FeaturedCollections />
+
+        {[products].map((product) => {
+          return (
+            <div>
+              {product.id}
+              <h1>hej</h1>
+            </div>
+          );
+        })}
       </Suspense>
     </Layout>
   );
 }
+
+
+const AllProducts = gql`
+  query AllProducts {
+    products(first: 12) {
+      edges {
+        node {
+          id
+          title
+          vendor
+          descriptionHtml
+        }
+      }
+    }
+  }
+`;
+
