59954e602a9a4da2ce38318fd4b268902a843cca
Amanda Hultén <amanda@avitusit.se>
2022-10-21 11:40:26 +0200

    Added popular collections section

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -4,6 +4,8 @@ import { Suspense } from 'react';
 import { Layout } from '../components/Layout.server';
 import Hero from '../components/Home/Hero.server';
 import Bestsellers from '../components/Home/Bestsellers.server';
+import PopularCollections from '../components/Home/PopularCollections.server';
+
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <Hero />
       <Suspense fallback={`Loading content...`}>
         <Bestsellers />
+        <PopularCollections />
       </Suspense>
     </Layout>
   );
