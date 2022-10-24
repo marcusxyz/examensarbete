5aff49b1291d801a6b0919f1211f4d58292cda14
Amanda Hultén <amanda@avitusit.se>
2022-10-24 10:40:57 +0200

    Fixed bestseller-page  enter the commit message for your changes. Lines starting

--- src/components/Home/Bestsellers.server.jsx
+++ src/components/Home/Bestsellers.server.jsx
@@ -1,8 +1,6 @@
 import { gql, useShopQuery, CacheLong } from '@shopify/hydrogen';
 import ProductCard from '../Product/ProductCard.server';
 
-import { ButtonLarge } from '../Elements/ButtonLarge';
-
 export default function Bestsellers() {
   const {
     data: { collection },
@@ -11,42 +9,6 @@ export default function Bestsellers() {
     cache: CacheLong(),
   });
 
-  // console.log('💫👾💫 DEBUGGING IN PROGRESS 💫👾💫');
-  // console.log(products.edges[0].node.variants.edges[0].node);
-  // console.log(products.edges);
-
-  return (
-    <section className='w-full'>
-      <h2 className='text-2xl my-4'>
-        Our bestsellers
-      </h2>
-
-      <div className='grid grid-flow-row gap-y-6 grid-cols-2 false sm:grid-cols-4'>
-        {products.edges.map((product) => {
-          const productTitle = product.node.title;
-          const price = product.node.variants.edges[0].node.priceV2.amount;
-          return (
-            <Link key={product.node.id} to={`/products/${product.node.handle}`} className='odd:border-r border-y border-black'>
-              <div className='grid gap-2'>
-                {product?.node.variants.edges[0].node.image && (
-                  <Image
-                    className='overflow-clip inline-block object-cover'
-                    width={'100%'}
-                    height={336}
-                    alt={`Image of ${product.node.title}`}
-                    data={product?.node.variants.edges[0].node.image}
-                  />
-                )}
-                <div className='px-2 pb-2'>
-                  <h2 className='text-sm mb-2'>
-                    {productTitle}
-                  </h2>
-                  <p className='text-md'>${price}</p>
-                </div>
-              </div>
-            </Link>
-          );
-        })}
   return (
     <section className='w-full grid pb-24'>
       <div className='flex flex-row justify-between items-center px-[10px] md:px-6 py-8 border-y border-black'>
@@ -63,10 +25,6 @@ export default function Bestsellers() {
 }
 const BESTSELLER_QUERY = gql`
   query Bestsellers {
-    products(first: 4) {
-      edges {
-        node {
-          # Get id, title and handle of the product
     collection(handle: "textures") {
       title
       handle
@@ -100,5 +58,3 @@ const BESTSELLER_QUERY = gql`
     }
   }
 `;
-
-
