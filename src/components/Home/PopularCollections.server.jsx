bec6247419243edb9440897634d31573f52ec9dc
Amanda Hultén <amanda@avitusit.se>
2022-10-24 09:35:38 +0200

    Fixed size on text

--- src/components/Home/PopularCollections.server.jsx
+++ src/components/Home/PopularCollections.server.jsx
@@ -12,18 +12,18 @@ export default function PopularCollections() {
 
     return (
         <section className="w-full my-20 px-2">
-            <h2 className="text-md">
+            <h2 className="text-md lg:text-xl">
                 POPULAR CATEGORIES
             </h2>
-            <div className='mt-10 max-w-md'>
+            <div className='mt-10 max-w-md md:max-w-lg lg:max-w-3xl xl:max-w-6xl'>
                 {collections.nodes.map((collection) => {
                     return (
                         <Link
                             key={collection.id}
                             to={`/collections/${collection.handle}`}
-                            className="inline-block after:content-[','] after:mr-2 after:text-black text-4xl font-medium last:after:content-['']"
+                            className="inline-block after:content-[','] after:mr-2 after:text-black text-4xl lg:text-5xl xl:text-6xl font-medium last:after:content-['']"
                         >
-                            <span className=''>{collection.title}</span>
+                            <span>{collection.title}</span>
                         </Link>
                     );
                 })}
