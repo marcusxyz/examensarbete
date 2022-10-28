d7ac0d24102d186eeb359211587a2557103ecf5d
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-10-28 12:14:05 +0200

    WIP Grid layout and styling for product page

--- src/components/Product/ProductDetails.client.jsx
+++ src/components/Product/ProductDetails.client.jsx
@@ -10,26 +10,23 @@ import {
 export default function ProductDetails({ product }) {
   return (
     <ProductOptionsProvider data={product}>
-      <section className='w-full overflow-x-hidden gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12'>
-        <div className='grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3'>
-          <div className='grid md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 md:w-full lg:col-span-2'>
-            <div className='md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded'>
+      <section className='w-full overflow-x-hidden gap-4 md:gap-8 grid border-b border-black'>
+        <div className='grid items-start md:grid-cols-2 lg:grid-cols-3 divide-x divide-black'>
+          <div className='grid md:grid-flow-row gap-[1px] md:p-0 md:overflow-x-auto md:grid-cols-2 md:w-full lg:col-span-2 bg-black'>
+            <div className='md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw]'>
               <ProductGallery media={product.media.nodes} />
             </div>
           </div>
-          <div className='sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-8 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]'>
-            <div className='grid gap-2'>
-              <h1 className='text-4xl font-bold leading-10 whitespace-normal'>
+          <div className='sticky h-full grid px-[10px] pt-6 md:pt-16 md:px-6 bg-white'>
+            <div className='pb-8 md:pb-14'>
+              <h1 className='text-[32px] md:text-7xl font-medium whitespace-normal'>
                 {product.title}
               </h1>
-              <span className='max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium'>
-                {product.vendor}
-              </span>
             </div>
             <ProductForm product={product} />
             <div className='mt-8'>
               <div
-                className='prose border-t border-gray-200 pt-6 text-black text-md'
+                className='prose border-t border-black pt-6 text-black text-md'
                 dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
               ></div>
             </div>
@@ -150,7 +147,7 @@ function ProductGallery({ media }) {
 
   return (
     <div
-      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
+      className={`grid gap-[1px] overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
     >
       {media.map((med, i) => {
         let extraProps = {};
@@ -175,8 +172,8 @@ function ProductGallery({ media }) {
         return (
           <div
             className={`${
-              i % 3 === 0 ? 'md:col-span-2' : 'md:col-span-1'
-            } snap-center card-image bg-white aspect-square md:w-full w-[80vw] shadow-sm rounded`}
+              i % 3 === 0 ? 'md:col-span-1' : 'md:col-span-1'
+            } snap-center card-image bg-white aspect-square md:w-full w-[80vw]`}
             key={med.id || med.image.id}
           >
             <MediaFile
