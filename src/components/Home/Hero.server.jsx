1b5a9369120cf2f67be5f8cd34d99a789ccc95f9
Amanda Hultén <amanda@avitusit.se>
2022-11-01 09:25:01 +0100

    Made Hero dynamic contentful

--- src/components/Home/Hero.server.jsx
+++ src/components/Home/Hero.server.jsx
@@ -10,10 +10,13 @@ export default function Hero() {
 
   console.log('💫👾💫 DEBUGGING IN PROGRESS 💫👾💫');
   console.dir(contentfulData, { depth: 5 });
-  console.log(contentfulData.headerCollection.items[0].image.url);
+  console.log(contentfulData.heroCollection.items[0].image.url);
 
-  const heroTitle = contentfulData.headerCollection.items[0].text;
-  const heroImg = contentfulData.headerCollection.items[0].image.url;
+  const heroTitle = contentfulData.heroCollection.items[0].title;
+  const heroParagraph = contentfulData.heroCollection.items[0].paragraph;
+  const heroText = contentfulData.heroCollection.items[0].buttonText;
+  const heroLink = contentfulData.heroCollection.items[0].buttonLink;
+  const heroImg = contentfulData.heroCollection.items[0].image.url;
 
   return (
     <>
@@ -32,14 +35,12 @@ export default function Hero() {
             <div>
               <h1 className='text-4xl lg:text-5xl xl:text-6xl'>{heroTitle}</h1>
               <p className='text-lg lg:w-[75%] mt-4'>
-                Let’s get close and personal. Our quality texture maps allow you
-                to capture stunning close-ups or epic wide-shots in your scenes.
-                Giving you the freedom to create the renders you want.
+                {heroParagraph}
               </p>
             </div>
 
             <div className='my-4'>
-              <ButtonSmall to='/' btnName='Explore high quality textures' />
+              <ButtonSmall to={heroLink} btnName={heroText} />
             </div>
           </div>
         </section>
@@ -50,9 +51,12 @@ export default function Hero() {
 
 const HERO_QUERY = gql`
   query GetHeroContent {
-    headerCollection {
+    heroCollection {
       items {
-        text
+        title,
+        paragraph,
+        buttonText,
+        buttonLink,
         image {
           url
         }
