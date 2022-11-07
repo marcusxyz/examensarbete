7aa3ed2a29c83d252e46f134f2fe6edd4e9e5a5b
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-11-07 11:59:10 +0100

    Desktop styling for popular-collections

--- src/components/Home/PopularCollections.server.jsx
+++ src/components/Home/PopularCollections.server.jsx
@@ -1,4 +1,5 @@
 import { Link, gql, useShopQuery, CacheLong } from '@shopify/hydrogen';
+import '../../index.css';
 
 export default function PopularCollections() {
   const {
@@ -11,7 +12,7 @@ export default function PopularCollections() {
   // console.log(collections.nodes[0]);
 
   return (
-    <section className='w-full my-20 px-2'>
+    <section className='w-full py-20 px-[10px] md:px-6 md:py-40'>
       <h2 className='text-md lg:text-xl'>POPULAR CATEGORIES</h2>
       <div className='mt-10 max-w-md md:max-w-lg lg:max-w-3xl xl:max-w-6xl'>
         {collections.nodes.map((collection) => {
@@ -19,9 +20,11 @@ export default function PopularCollections() {
             <Link
               key={collection.id}
               to={`/collections/${collection.handle}`}
-              className="inline-block after:content-[','] after:mr-2 after:text-black text-4xl lg:text-5xl xl:text-6xl font-medium last:after:content-['']"
+              className="inline-block after:content-[',\00a0\00a0'] after:text-black text-4xl lg:text-5xl xl:text-6xl font-medium last:after:content-['']"
             >
-              <span>{collection.title}</span>
+              <span className='link link-underline link-underline-black leading-tight'>
+                {collection.title}
+              </span>
             </Link>
           );
         })}
@@ -32,7 +35,7 @@ export default function PopularCollections() {
 
 const QUERY = gql`
   query Collections {
-    collections(first: 8, sortKey: UPDATED_AT) {
+    collections(first: 8, sortKey: RELEVANCE) {
       nodes {
         id
         title
