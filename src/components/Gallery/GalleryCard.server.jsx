620619a86e35e3b8ba1002d68c66310c3f386f87
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-11-09 15:37:22 +0100

    alt tags to images + styling adjustments

--- src/components/Gallery/GalleryCard.server.jsx
+++ src/components/Gallery/GalleryCard.server.jsx
@@ -14,7 +14,7 @@ export default function GalleryCard({ src, alt, name, url, fabric, btnText }) {
       <div className='flex justify-between gap-3 h-[10%]'>
         <div className='mt-2'>
           <h2 className='lg:text-sm lg:font-medium'>{name}</h2>
-          <Link to={url} className='text-sm'>
+          <Link to={url} className='text-sm hover:underline'>
             {btnText}
           </Link>
         </div>
