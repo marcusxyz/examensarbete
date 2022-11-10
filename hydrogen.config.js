ed7f537516c4b484b3f29428e0d5aeabf12a03a4
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-11-10 10:34:29 +0100

    Removed process.env

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,9 +3,7 @@ import { defineConfig } from '@shopify/hydrogen/config';
 export default defineConfig({
   shopify: {
     storeDomain: 'texturesupply.myshopify.com',
-    storefrontToken:
-      process?.env['PUBLIC_STOREFRONT_API_TOKEN'] ||
-      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
+    storefrontToken: Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
     storefrontApiVersion: '2022-07',
   },
   logger: {
