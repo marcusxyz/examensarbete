24b15d0e217c2ae0176bd70a901ba7ca7b120a83
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-11-10 10:43:25 +0100

    Deploy test

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from '@shopify/hydrogen/config';
 export default defineConfig({
   shopify: {
     storeDomain: 'texturesupply.myshopify.com',
-    storefrontToken: Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
+    storefrontToken: '438ca477e04408eaf92685f7489be0b7',
     storefrontApiVersion: '2022-07',
   },
   logger: {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
 
 export default defineConfig({
-  plugins: [hydrogen(), netlifyPlugin()],
+  plugins: [hydrogen()],
 });
