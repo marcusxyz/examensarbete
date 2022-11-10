6e3a0519f09de4a555f3683c290882a01271b344
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-11-10 10:29:25 +0100

    env variables

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from '@shopify/hydrogen/config';
 export default defineConfig({
   shopify: {
     storeDomain: 'texturesupply.myshopify.com',
-    storefrontToken: '438ca477e04408eaf92685f7489be0b7',
+    storefrontToken:
+      process?.env['PUBLIC_STOREFRONT_API_TOKEN'] ||
+      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN,
     storefrontApiVersion: '2022-07',
   },
   logger: {
